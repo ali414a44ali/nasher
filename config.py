c98389bc875cb62269548c18ef0e167778f98666
ali414a44ali <ali414a44ali@gmail.com>
2025-11-07 15:47:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 APP_ID = os.environ.get("APP_ID", "9043884")
 APP_HASH = os.environ.get("APP_HASH", "733596982db7e73247489b26b1a55662")
 SESSION = os.environ.get("SESSION","جلسة تليثون")
-fraon = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
-fraon.start()
+BiLaL = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
+BiLaL.start()
